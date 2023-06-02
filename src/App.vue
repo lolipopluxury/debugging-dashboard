4ffe0e1dbba2eb70cd0cf4cceef61bd30d4fa382
lolipopluxury <songyupeng@kanzhun.com>
2023-06-02 09:39:51 +0800

    propject init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <ElHeader class="appHeader">H5 测试数据档案馆</ElHeader>
+  <ElContainer>
+    <ElAside width="200px">Aside</ElAside>
+    <ElContainer>
+      <ElMain>
+        <RouterView />
+      </ElMain>
+    </ElContainer>
+  </ElContainer>
+</template>
+
+<style lang="less" scoped>
+.appHeader {
+  color: #fff;
+  font-size: 0.24rem;
+  font-weight: bold;
+  background: linear-gradient(135deg, #59ae6a, #48ae96, #60d9ec, #65a5d5, #9491e0, #c382a4);
+  display: flex;
+  align-items: center;
+}
+</style>
