90c4fdf011fdc71cee36043c24362559ede6f22a
lolipopluxury <songyupeng@kanzhun.com>
2023-06-02 18:09:55 +0800

    home page init

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,11 @@
 
 <style lang="less" scoped>
 .appHeader {
-  color: #fff;
-  font-size: 0.24rem;
-  font-weight: bold;
-  background: linear-gradient(135deg, #59ae6a, #48ae96, #60d9ec, #65a5d5, #9491e0, #c382a4);
   display: flex;
   align-items: center;
+  font-weight: bold;
+  font-size: 0.24rem;
+  color: #fff;
+  background: linear-gradient(135deg, #59ae6a, #48ae96, #60d9ec, #65a5d5, #9491e0, #c382a4);
 }
 </style>
