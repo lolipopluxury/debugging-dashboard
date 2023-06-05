6c40a5344b9aa78ae3330e0ccc03ea51cc648ce4
lolipopluxury <songyupeng@kanzhun.com>
2023-06-05 16:36:51 +0800

    data collection and display update

--- src/views/tbs-research/TbsOverview.vue
+++ src/views/tbs-research/TbsOverview.vue
@@ -1,27 +1,101 @@
 <template>
-  <div>
-    tbs overview
-    <p>{{ resource }}</p>
-  </div>
+  <ElTabs v-model="tabActive" type="border-card">
+    <ElTabPane label="CSS" name="css">
+      <ElTable :data="displayDataCSS" border style="width: 100%">
+        <ElTableColumn prop="property" label="Property" width="180" />
+        <ElTableColumn label="Data With TBS">
+          <template #default="scope">
+            <ElText type="primary">当前数据量：{{ scope.row.dataWithTBS.length }}</ElText>
+            <p v-for="item in scope.row.dataWithTBS" :key="item._id">{{ item }}</p>
+          </template>
+        </ElTableColumn>
+        <ElTableColumn label="Data Without TBS">
+          <template #default="scope">
+            <ElText type="primary">当前数据量：{{ scope.row.dataWithoutTBS.length }}</ElText>
+            <p v-for="item in scope.row.dataWithoutTBS" :key="item._id">{{ item }}</p>
+          </template>
+        </ElTableColumn>
+      </ElTable>
+    </ElTabPane>
+    <ElTabPane label="ES6" name="js">
+      <ElTable :data="displayDataES6" border style="width: 100%">
+        <ElTableColumn prop="property" label="Property" width="180" />
+        <ElTableColumn label="Data With TBS">
+          <template #default="scope">
+            <ElText type="primary">当前数据量：{{ scope.row.dataWithTBS.length }}</ElText>
+            <p v-for="item in scope.row.dataWithTBS" :key="item._id">{{ item }}</p>
+          </template>
+        </ElTableColumn>
+        <ElTableColumn label="Data Without TBS">
+          <template #default="scope">
+            <ElText type="primary">当前数据量：{{ scope.row.dataWithoutTBS.length }}</ElText>
+            <p v-for="item in scope.row.dataWithoutTBS" :key="item._id">{{ item }}</p>
+          </template>
+        </ElTableColumn>
+      </ElTable>
+    </ElTabPane>
+  </ElTabs>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { apiFetchTbsData } from '../../services'
+import { cssCategory, es6Category } from '../../models/tbs/index'
 
-const resource = ref([])
+const tabActive = ref('css')
 
-const total = ref(0)
+const resourceCSS = ref({})
+
+const resourceES6 = ref({})
+
+const displayDataCSS = ref([])
+
+const displayDataES6 = ref([])
 
 const page = 1
 
 const pageSize = 1000000
 
 const fetchData = async () => {
-  const res = await apiFetchTbsData({ params: { category: 'css', pageSize, page } })
-  resource.value = res.list
-  total.value = res.total
+  const cssData = await apiFetchTbsData({ params: { category: 'css', pageSize, page } })
+  cssData.list.forEach((item) => {
+    item.isTbs
+      ? resourceCSS.value[item.property].dataWithTBS.push(item)
+      : resourceCSS.value[item.property].dataWithoutTBS.push(item)
+  })
+  const es6Data = await apiFetchTbsData({ params: { category: 'js', pageSize, page } })
+  es6Data.list.forEach((item) => {
+    item.isTbs
+      ? resourceES6.value[item.property].dataWithTBS.push(item)
+      : resourceES6.value[item.property].dataWithoutTBS.push(item)
+  })
+  fetchDataCallback()
 }
 
+const dataInit = () => {
+  cssCategory.forEach((key) => {
+    resourceCSS.value[key] = { dataWithTBS: [], dataWithoutTBS: [] }
+  })
+  es6Category.forEach((key) => {
+    resourceES6.value[key] = { dataWithTBS: [], dataWithoutTBS: [] }
+  })
+}
+
+const fetchDataCallback = () => {
+  cssCategory.forEach((key) => {
+    displayDataCSS.value.push({
+      property: key,
+      ...resourceCSS.value[key]
+    })
+  })
+  es6Category.forEach((key) => {
+    displayDataES6.value.push({
+      property: key,
+      ...resourceES6.value[key]
+    })
+  })
+}
+
+dataInit()
 fetchData()
 </script>
